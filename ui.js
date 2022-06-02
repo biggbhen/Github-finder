a8f0f1f147bf34dd7a4017eff70ed01a2641b971
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-02 11:53:26 +0100

    done

--- ui.js
+++ ui.js
@@ -16,5 +16,11 @@ class UI {
     document.querySelector('.timeCreated').textContent = new Date(
       data.profileData.created_at
     ).toLocaleDateString('en-US');
+
+    document.querySelector('.location').textContent = data.profileData.location;
+    document.querySelector('.website').textContent = data.profileData.html_url;
+    document.querySelector('.twitter').textContent =
+      data.profileData.twitter_username;
+    document.querySelector('.company').textContent = data.profileData.company;
   }
 }
