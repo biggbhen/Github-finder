12710afe25d0b7d0f6f220eaa917b1eed01ead01
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-07 23:18:36 +0100

    checked for a data

--- github.js
+++ github.js
@@ -8,6 +8,7 @@ class Github {
       `https://api.github.com/users/${user}?client_id = ${this.client_id}&client_secret=${this.client_secret}`
     );
     const profileData = await profileResponse.json();
+    // console.log(profileData.followers_url);
     return {
       profileData,
     };
